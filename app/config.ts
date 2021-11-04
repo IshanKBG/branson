9e3f95986a73c33fd954a9d70c80deca5c085372
IshanKBG <jaiswalishan701@gmail.com>
2021-11-04 12:14:20 +0530

    removed Docker support and added a run.json file for shortcuts using trex

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 import { Config } from "~/types/mod.ts";
 import { config as env } from "env/mod.ts";
-const token = env().TOKEN || Deno.env.get("TOKEN");
+const token = env().DISCORD_TOKEN || Deno.env.get("DISCORD_TOKEN");
 if (!token) throw new Error("NO TOKEN");
 export const config: Config = {
   token,
