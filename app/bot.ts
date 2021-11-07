898a5b560b45bc3925f33984edcce967f371bf63
IshanKBG <jaiswalishan701@gmail.com>
2021-11-07 14:12:03 +0530

    Added oak middleware framework for creating rest apis

--- app/bot.ts
+++ app/bot.ts
@@ -1,5 +1,10 @@
+import { Application } from 'oak'
 import { DiscordActivityTypes, editBotStatus, startBot } from "discord/mod.ts";
 import { config } from "~/config.ts";
+
+const app = new Application();
+
+
 startBot({
   token: config.token, // config.token,
   intents: ["Guilds", "GuildMessages"],
@@ -19,3 +24,9 @@ startBot({
     },
   },
 });
+
+app.use((ctx) => {
+  ctx.response.body = "Hello world!";
+});
+
+await app.listen({ port: 8000});
